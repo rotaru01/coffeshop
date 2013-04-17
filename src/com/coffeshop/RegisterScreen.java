508d7d942d6171d7dedf09b2bc5081d5c15cd063
rotaru01 <rotaru01@yahoo.com>
2013-04-17 21:35:00 +0300

    registration and login screen

---
+++ src/com/coffeshop/RegisterScreen.java
@@ -0,0 +1,19 @@
+package com.coffeshop;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class RegisterScreen extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.register_screen);
+	}
+
+}
